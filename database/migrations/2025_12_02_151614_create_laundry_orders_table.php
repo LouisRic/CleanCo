629de68603888c3ee66a6898ac35669f164c10ec
shrlne <sharlenesunn@gmail.com>
2025-12-02 22:59:06 +0700

    Add migrations

---
+++ database/migrations/2025_12_02_151614_create_laundry_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laundry_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')->constrained('accounts')->onDelete('cascade');
+            $table->foreignId('laundry_type_id')->constrained('laundry_types')->onDelete('restrict');
+
+            $table->date('order_date');
+            $table->date('pickup_date')->nullable();
+
+            $table->float('weight_kg');
+            $table->integer('price_per_kg');
+            $table->integer('total_price');
+
+            $table->text('notes')->nullable();
+
+            $table->enum('payment_status', ['paid', 'unpaid'])->default('unpaid');
+            $table->enum('laundry_status', ['process', 'washed', 'ready', 'taken'])->default('process');
+            $table->enum('pickup_status', ['not_taken', 'taken'])->default('not_taken');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laundry_orders');
+    }
+};
---
+++ database/migrations/2025_12_02_153904_create_vouchers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->sting('name');
+            $table->enum('type', ['percentage', 'fixed']); // diskonnya pake persen atau nominal
+            $table->integer('value'); // 10% atau 10.000
+            $table->integer('minimum_spend')->default(0); // min hrs spend brp buat pake voucher
+            $table->integer('points_required');
+            $table->date('valid_from');
+            $table->date('valid_until');
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
